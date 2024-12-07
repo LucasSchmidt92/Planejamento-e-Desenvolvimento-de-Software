628ea9ba25c487964fa1353e445666c37241f927
LucasSchmidt92 <lucas_schmidt92@hotmail.com>
2024-12-06 21:55:30 -0300

    Add files via upload

---
+++ 06-12-24/main (1).js
@@ -0,0 +1,67 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// console.log('Hello World');
+
+// let A = true
+// let B = false
+// let resultado = A && B
+// console.log (A, '&&', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = true
+// let B = false
+// let resultado = A != B
+// console.log (A, '!=', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = true
+// let B = false
+// let resultado = A || B
+// console.log (A, '||', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = true
+// let B = true
+// let resultado = A == B
+// console.log (A, '==', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = 3
+// let B = 5
+// let resultado = A > B
+// console.log (A, '>', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = 3
+// let B = 5
+// let resultado = A < B
+//console.log (A, '<', B , '=', resultado)
+
+//console.log('Hello World');
+//let A = 3
+//let B = 5
+//let resultado = A >= B
+//console.log (A, '>=', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = 3
+// let B = 5
+// let resultado = A <= B
+// console.log (A, '<==', B , '=', resultado)
+
+// let idade = 2 
+
+// let status = idade >= 18 ? 'adulto' : 'menor de idade'
+
+// console.log(status);
+
+var palavra1 = 'alfa';
+var palavra2 = 'beto';
+var resultado = palavra1+palavra2;
+console.log(resultado)
+
---
+++ 06-12-24/main.js
@@ -0,0 +1,61 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// console.log('Hello World');
+
+// let A = true
+// let B = false
+// let resultado = A && B
+// console.log (A, '&&', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = true
+// let B = false
+// let resultado = A != B
+// console.log (A, '!=', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = true
+// let B = false
+// let resultado = A || B
+// console.log (A, '||', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = true
+// let B = true
+// let resultado = A == B
+// console.log (A, '==', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = 3
+// let B = 5
+// let resultado = A > B
+// console.log (A, '>', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = 3
+// let B = 5
+// let resultado = A < B
+//console.log (A, '<', B , '=', resultado)
+
+//console.log('Hello World');
+//let A = 3
+//let B = 5
+//let resultado = A >= B
+//console.log (A, '>=', B , '=', resultado)
+
+// console.log('Hello World');
+// let A = 3
+// let B = 5
+// let resultado = A <= B
+// console.log (A, '<==', B , '=', resultado)
+
+
+
+
+
+
